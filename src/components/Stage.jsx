6c65ef9472b10f7f49d40af63a4ce7493c74eb78
arpitashrivastav <arpitasrivastava26481@gmail.com>
2023-04-15 08:44:38 +0530

    Add style to Stage using StyledStage

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -1,8 +1,11 @@
 import Cell from './Cell';
+import { StyledStage } from './styles/StyledStage';
 
 const Stage = ({ stage }) => {
   return (
-    <div>{stage.map((row) => row.map((cell) => <Cell type={cell[0]} />))}</div>
+    <StyledStage width={stage[0].length} height={stage.length}>
+      {stage.map((row) => row.map((cell) => <Cell type={cell[0]} />))}
+    </StyledStage>
   );
 };
 
