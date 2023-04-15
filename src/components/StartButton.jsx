c40e5de83a9a11498a6518084dbb185bb4148044
arpitashrivastav <arpitasrivastava26481@gmail.com>
2023-04-15 08:51:00 +0530

    Add style to StartButton using StyledStartButton

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,5 +1,7 @@
-const StartButton = () => {
-  return <div>StartButton</div>;
+import { StyledStartButton } from './styles/StyledStartButton';
+
+const StartButton = ({ callBack }) => {
+  return <StyledStartButton onClick={callBack}>StartButton</StyledStartButton>;
 };
 
 export default StartButton;
