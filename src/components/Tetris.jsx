2d01c4c71e7a1df690f6c6154ffe94ba22e13da1
arpitashrivastav <arpitasrivastava26481@gmail.com>
2023-04-15 08:42:19 +0530

    Add styles to Tetris using StyledTetris

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -2,20 +2,23 @@ import { createStage } from '../gameHelpers';
 import Display from './Display';
 import Stage from './Stage';
 import StartButton from './StartButton';
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const STAGE = createStage();
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={STAGE} />
-      <aside>
-        <Display text="Score" />
-        <Display text="Rows" />
-        <Display text="Level1" />
-      </aside>
-      <StartButton />
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={STAGE} />
+        <aside>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level1" />
+        </aside>
+        <StartButton />
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
