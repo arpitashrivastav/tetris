814c756e9129a13976277581541c84ba347b9197
arpitashrivastav <81864506+arpitashrivastav@users.noreply.github.com>
2023-05-01 14:56:26 +0530

    Move to vite for fast deployment

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,7 @@
 import { StyledDisplay } from './styles/StyledDisplay';
 
-const Display = ({ gameOver, text }) => {
-  return <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>;
-};
+const Display = ({ gameOver, text }) => (
+  <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
+);
 
 export default Display;
